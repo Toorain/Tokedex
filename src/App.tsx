3278aaeca8e8c3232da450b873868c3f81029444
Maxence WILMES <maxencewilmes@gmail.com>
2023-03-13 22:14:26 +0100

    Dark/light mode and first draw of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import MainList from "./MainList";
+import './darkMode.css';
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+    },
+  });
+
+  const [theme, setTheme] = useState(darkTheme);
+
+  const toggleTheme = () => {
+    if (theme.palette.mode === 'dark') {
+      setTheme(lightTheme);
+    } else {
+      setTheme(darkTheme);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={'App'}>
+        <MainList />
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <CssBaseline />
+      </div>
+    </ThemeProvider>
   );
 }
 
